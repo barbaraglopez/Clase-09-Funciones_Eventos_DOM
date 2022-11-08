a477afaef3bf4587a30bd63f2963e434995dd452
barbaraglopez <barbaralopezbbbb@gmail.com>
2022-11-08 00:49:35 -0300

    primer commit

---
+++ Js/script.js
@@ -0,0 +1,80 @@
+/* function saludar() {
+    console.log("Hola Mundo!");
+}
+
+saludar(); */
+
+/* SIN PARAMETROS */
+/* const adios = () => {
+    console.log(`adios`);
+};
+
+adios(); */
+
+/* CON PARAMETROS */
+/* const hola = (nombre) => {
+    console.log(`Hola ${nombre}`)
+}
+
+hola(`barbara`); */
+
+/* RETURN */
+/* const sumar = (numero1, numero2) => {
+    return numero1 + numero2;
+}
+ */
+
+/* const sumar =(numero1, numero2)=> {
+    return numero1 + numero2;
+}
+let resultado = sumar(40, 2);
+console.log("40 + 2 es igual a: " + resultado); */
+
+/* SCOPE */
+/* Scope local */
+/* const sumarEdad = (edad)=> {
+    let num = 1;
+    return edad + num;
+}
+
+console.log(num) */
+/* Es cuando se puede acceder a una variable únicamente en
+cierta parte del código. Por ejemplo, cuando variable está
+declarada dentro de un bloque o una función. */
+
+
+/* Scope global */
+/* let num = 1;
+function sumarEdad(edad) {
+return edad + num;
+}
+
+console.log(num) */
+/* Es cuando la variable está declarada fuera de una función o de
+un bloque. Se puede acceder a éstas desde cualquier parte del
+código */
+
+
+/* 
+EVENTOS */
+
+/* let button = document.getElementById("btn"); */
+
+/* button.addEventListener("click", () => {
+    console.log('Hola!')
+}) */
+
+/* const respuestaClick = () =>{
+    console.log("Hola!");
+} */
+
+/* button.addEventListener("click", respuestaClick); */
+
+/* let button = document.getElementById("btn")
+btn.onclick = () => {
+    console.log("hola")
+} */
+
+
+
+
